9d6507495d8256794032e1fe317a4b8bf518a989
eliran <manzeli@me.com>
2020-08-09 14:55:40 +0300

    Cart Item Component + update count number by items quantity

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import {ReactComponent as ShoopingIcon} from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss';
 import {connect} from 'react-redux';
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden} >
-        <ShoopingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <ShoopingIcon className='shopping-icon'  />
+<span className='item-count'>{itemCount}</span>
     </div>
 );
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
-export default connect (null,
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+});
+export default connect (mapStateToProps,
     mapDispatchToProps)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,7 +5,7 @@ import {connect} from 'react-redux'
 import {ReactComponent as Logo} from '../../assets/suit.svg';
 import {auth} from '../../firebase/firebase.util';
 import CartIcon from '../cart-icon/cart-icon.component';
-import Cart from '../cart-dropdown/cart-dropdown.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 const Header = ({currentUser, hidden}) => (
     <div className='header'>
@@ -22,7 +22,7 @@ const Header = ({currentUser, hidden}) => (
             }
             <CartIcon  />
         </div>
-       {hidden ? null : <Cart />}
+       {hidden ? null : <CartDropdown />}
     </div>
 );
 const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
