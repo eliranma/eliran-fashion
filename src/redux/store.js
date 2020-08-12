e16095403d1e4188fd18ce9d7a48c1f332cc3891
eliran <manzeli@me.com>
2020-08-12 11:08:44 +0300

    second try

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ import rootReducer from './root-reducer';
 import {persistStore} from 'redux-persist';
 
 const middleWares =[logger];
+if (proccess.env.NODE_ENV === "development")
 
 export const store = createStore(rootReducer,applyMiddleware(...middleWares));
 export const persistor = persistStore(store);
